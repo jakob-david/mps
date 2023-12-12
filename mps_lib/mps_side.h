88cb0f8b57fd981c49a5b0537d280547c7d71c5a
Jakob <jakob.d.neuhauser@gmail.com>
2023-12-12 20:11:00 +0100

    code beautified.

--- mps_lib/mps_side.h
+++ mps_lib/mps_side.h
@@ -16,20 +16,52 @@ private:
 
 public:
     mps_side(int mantisse, int exponent, T value);
+    ~mps_side();
 
     [[nodiscard]] T getSideCalculationValue() const;
 };
 
-template<typename T>
-T mps_side<T>::getSideCalculationValue() const {
-    return this->side_calculation_value;
-}
 
+
+
+
+
+
+// constructors and destructor
+//-------------------------------
+
+/**
+ * Constructor for the multiprecision simulator including a side calculation.
+ *
+ * @param mantisse Mantisse of the floating point representation.
+ * @param exponent Exponent of the floating point representation.
+ * @param value Value of the floating point number.
+ */
 template<typename T>
 mps_side<T>::mps_side(int mantisse, int exponent, T value):mps(mantisse, exponent, value) {
 
     this->side_calculation_value = value;
 }
 
+/**
+ * Destructor of mps_side
+ */
+template<typename T>
+mps_side<T>::~mps_side() = default;
+//-------------------------------
+
+// getter
+//-------------------------------
+
+/**
+ * Returns the value with is calculated in addition to the calculations concerning the calculator.
+ *
+ * @return side calculation value.
+ */
+template<typename T>
+T mps_side<T>::getSideCalculationValue() const {
+    return this->side_calculation_value;
+}
+//-------------------------------
 
 #endif //MPS_MPS_SIDE_H
