6fe9af83643fcb9fa61051ded0d112654e75f283
Jakob <jakob.d.neuhauser@gmail.com>
2024-05-24 14:59:50 +0200

    print sizeof added

--- mpe_lib/mpe.cpp
+++ mpe_lib/mpe.cpp
@@ -548,9 +548,6 @@ vector<long double> mpe::evaluateDivisionExponent() const {
 //-------------------------------
 vector<long double> mpe::evaluateAdditionDouble() const {
 
-    printf("%lu\n", sizeof(double));
-    printf("%lu\n", sizeof(long double));
-
     // variable for python multithreading.
     py::gil_scoped_release release;
 
